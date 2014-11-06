f62427f66a0c66808df332231bd9ae30e7570761
adlawson <adlawson@gmail.com>
2014-11-06 20:22:34 +0000

    Add exercise 2.2

---
+++ src/main/scala/com/github/adlawson/fpinscala/chapter02/Polymorphic.scala
@@ -0,0 +1,13 @@
+package com.github.adlawson.fpinscala.chapter02
+
+object Polymorphic {
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(n: Int, prev: A): Boolean =
+      if (as.length == n) true
+      else if (ordered(as(n), prev)) go(n+1, as(n))
+      else false
+    if (as.length == 0) true
+    else go(1, as(0))
+  }
+}
