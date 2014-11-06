69a4352701dac2bb6a93911512b7bb94f702b82b
adlawson <adlawson@gmail.com>
2014-11-06 20:22:20 +0000

    Add exercise 2.1

---
+++ src/main/scala/com/github/adlawson/fpinscala/chapter02/HigherOrder.scala
@@ -0,0 +1,12 @@
+package com.github.adlawson.fpinscala.chapter02
+
+object MyModule {
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, prev: Int, now: Int): Int = {
+      if (0 == n) prev
+      else go(n-1, now, prev+now)
+    }
+    go(n, 0, 1)
+  }
+}
